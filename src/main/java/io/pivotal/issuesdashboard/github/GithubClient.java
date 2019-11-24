37dad6ad97a56cfc21a62a892152ef65991ad3c9
Hirak Bhattacharjee <bh.hirak@gmail.com>
2019-11-24 21:06:17 +0530

    GithubAppTokenInterceptor

--- src/main/java/io/pivotal/issuesdashboard/github/GithubClient.java
+++ src/main/java/io/pivotal/issuesdashboard/github/GithubClient.java
@@ -5,15 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import io.pivotal.issuesdashboard.GithubProperties;
+
 @Component
 public class GithubClient {
 	
 	private static final String EVENT_ISSUE_URL = "https://api.github.com/repos/{owner}/{repo}/issues/events";
 
     private final RestTemplate restTemplate;
+    
 
-	public GithubClient(RestTemplateBuilder restTemplateBuilder) {
-		this.restTemplate = restTemplateBuilder.build();
+	public GithubClient(RestTemplateBuilder restTemplateBuilder, GithubProperties properties) {
+		this.restTemplate = restTemplateBuilder
+				.additionalInterceptors(new GithubAppTokenInterceptor(properties.getToken()))
+				.build();
 	}
     
     public ResponseEntity<RepositoryEvent[]> fetchEvents(String orgName, String repoName){
